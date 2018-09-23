4d789df42cab7debd9a1d4c1fd02beef1122f512
Neha Neha <neha.neha@sap.com>
2018-09-23 10:56:41 +0200

    inital commit

---
+++ src/de/openhpi/capstone1/starter/Main.java
@@ -0,0 +1,10 @@
+package de.openhpi.capstone1.starter;
+
+class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Eclipse");
+    }
+
+}
